c0bbd098641cab23a2a4648de66359639b59c1ce
evenmn <evenmn@fys.uio.no>
2025-08-07 20:29:05 +0200

    Bris-inference properly tested for ERA5-o96
    Signed-off-by: evenmn <evenmn@fys.uio.no>

--- src/ERA5-o96/bris-inference/jobscript.sh
+++ src/ERA5-o96/bris-inference/jobscript.sh
@@ -2,15 +2,14 @@
 
 #SBATCH -A DestE_330_25
 #SBATCH -p boost_usr_prod
-# #SBATCH -q boost_qos_dbg
 #SBATCH --nodes=20
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=0
 #SBATCH --time=24:00:00
-#SBATCH --job-name=ERA5-o96-infer
-#SBATCH --output=logs/ERA5-o96_infer.out
+#SBATCH --job-name=infer
+#SBATCH --output=logs/infer.out
 set -x
 
 export HYDRA_FULL_ERROR=1
