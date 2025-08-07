60a8655e806a28891c5f515a73ad36a438374b64
evenmn <evenmn@fys.uio.no>
2025-08-07 14:38:55 +0200

    Anemoi inference for IFS-N320_MEPS-2p5km was properly tested
    Signed-off-by: evenmn <evenmn@fys.uio.no>

--- src/IFS-N320_MEPS-2p5km/anemoi-inference/jobscript.sh
+++ src/IFS-N320_MEPS-2p5km/anemoi-inference/jobscript.sh
@@ -9,12 +9,11 @@
 #SBATCH --mem=0
 #SBATCH --time=02:00:00
 #SBATCH --job-name=ERA5-n320-infer
-#SBATCH --output=logs/ERA5-n320_infer.out
-# #SBATCH --output=logs/ERA5-n320_infer_%a.out
-# #SBATCH --array=0-2
+#SBATCH --output=logs/infer_%a.out
+#SBATCH --array=0-2  # specify number of members here
 set -x
 
 export HYDRA_FULL_ERROR=1
 source /leonardo_work/DestE_330_25/enordhag/venvs/inference/bin/activate
 
-srun anemoi-inference run config.yaml date=2023-01-01T00
+srun anemoi-inference run config.yaml date=2023-01-01T00 output.extract_lam.output.netcdf=netcdf/test_$SLURM_ARRAY_TASK_ID.nc
