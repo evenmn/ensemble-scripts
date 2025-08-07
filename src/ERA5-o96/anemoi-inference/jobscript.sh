92685affda18265f17220c0ee5e642d624e09649
evenmn <evenmn@fys.uio.no>
2025-08-07 15:48:08 +0200

    Tested anemoi-inference for ERA5-o96
    Signed-off-by: evenmn <evenmn@fys.uio.no>

--- src/ERA5-o96/anemoi-inference/jobscript.sh
+++ src/ERA5-o96/anemoi-inference/jobscript.sh
@@ -8,13 +8,12 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=0
 #SBATCH --time=02:00:00
-#SBATCH --job-name=ERA5-n320-infer
-#SBATCH --output=logs/ERA5-n320_infer.out
-# #SBATCH --output=logs/ERA5-n320_infer_%a.out
-# #SBATCH --array=0-2
+#SBATCH --job-name=ERA5-o96-infer
+#SBATCH --output=logs/infer_%a.out
+#SBATCH --array=0-2  # specify the number of members here
 set -x
 
 export HYDRA_FULL_ERROR=1
 source /leonardo_work/DestE_330_25/enordhag/venvs/inference/bin/activate
 
-srun anemoi-inference run config.yaml date=2022-01-02T00
+srun anemoi-inference run config.yaml date=2022-01-02T00 output.netcdf=netcdf/test_$SLURM_ARRAY_TASK_ID.nc
