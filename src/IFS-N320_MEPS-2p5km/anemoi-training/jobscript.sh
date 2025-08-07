ff0f741e2ebbedb00ffe35835d940ff1797be755
evenmn <evenmn@fys.uio.no>
2025-08-07 16:05:41 +0200

    Tested anemoi-training for IFS-N320_MEPS-2p5km
    Signed-off-by: evenmn <evenmn@fys.uio.no>

--- src/IFS-N320_MEPS-2p5km/anemoi-training/jobscript.sh
+++ src/IFS-N320_MEPS-2p5km/anemoi-training/jobscript.sh
@@ -2,19 +2,17 @@
 
 #SBATCH -A DestE_330_25
 #SBATCH -p boost_usr_prod
-# #SBATCH -q boost_qos_bprod
-#SBATCH --nodes=32
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=0
-#SBATCH --time=24:00:00
+#SBATCH --time=00:30:00
 #SBATCH --job-name=MEPS-ENS
-#SBATCH --output=logs/IFS-n320_MEPS-2p5km_smallerlr_2.out
-# #SBATCH --dependency=singleton
+#SBATCH --output=logs/IFS-n320_MEPS-2p5km_1.out
 set -x
 
 export HYDRA_FULL_ERROR=1
-source /leonardo_work/DestE_330_25/enordhag/venv/bin/activate
+source /leonardo_work/DestE_330_25/enordhag/venvs/training/bin/activate
 
 srun anemoi-training train --config-name=config.yaml
