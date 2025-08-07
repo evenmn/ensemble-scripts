fbdc974d5223228adc0ca2c086a2b0780b7d8d57
evenmn <evenmn@fys.uio.no>
2025-08-07 20:57:34 +0200

    Changed job-name
    Signed-off-by: evenmn <evenmn@fys.uio.no>

--- src/IFS-N320_MEPS-2p5km/anemoi-training/jobscript.sh
+++ src/IFS-N320_MEPS-2p5km/anemoi-training/jobscript.sh
@@ -2,14 +2,14 @@
 
 #SBATCH -A DestE_330_25
 #SBATCH -p boost_usr_prod
-#SBATCH --nodes=4
+#SBATCH --nodes=32
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=0
-#SBATCH --time=00:30:00
-#SBATCH --job-name=MEPS-ENS
-#SBATCH --output=logs/IFS-n320_MEPS-2p5km_1.out
+#SBATCH --time=24:00:00
+#SBATCH --job-name=IFS-N320_MEPS-2p5km
+#SBATCH --output=logs/IFS-N320_MEPS-2p5km_1.out
 set -x
 
 export HYDRA_FULL_ERROR=1
