9663c892e2bdea8db986d57c6426459cb88f35a3
evenmn <evenmn@fys.uio.no>
2025-10-28 13:26:14 +0100

    Added CRPS-FFT config
    Signed-off-by: evenmn <evenmn@fys.uio.no>

---
+++ src/IFS-N320_MEPS-2p5km-CRPS-FFT/anemoi-training/jobscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -A DestE_330_25 #EUHPC_R04_079
+#SBATCH -p boost_usr_prod
+# #SBATCH -q boost_qos_dbg
+#SBATCH --nodes=32
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=0
+#SBATCH --time=24:00:00
+#SBATCH --job-name=11_refinement
+#SBATCH --output=logs/crps-fft_1.out
+set -x
+
+export HYDRA_FULL_ERROR=1
+source /leonardo_work/DestE_330_25/enordhag/venvs/training/bin/activate
+
+srun anemoi-training train --config-name=config.yaml
